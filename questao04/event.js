4de7d76949cec5dc0cc10b2d9c7af0c240a3bc4f
Janilson Junior <janilsonjrdias@gmail.com>
2019-03-10 11:21:41 -0300

    Questão 04

---
+++ questao04/event.js
@@ -0,0 +1,11 @@
+function exibir() {
+    document.getElementById('paragrafo').style.display = 'block';
+    document.getElementById('esconder').style.display = 'block';
+    document.getElementById('exibir').style.display = 'none'
+}
+
+function esconder() {
+    document.getElementById('paragrafo').style.display = 'none';
+    document.getElementById('esconder').style.display = 'none';
+    document.getElementById('exibir').style.display = 'block';
+}
